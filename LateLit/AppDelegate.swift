5d1bf9be5883ad9d56ee78969204839d434a5655
Daniel Khromov <danielkhromov@gmail.com>
2020-02-10 00:47:28 +0300

    Firebase(not connected to server)

--- LateLit/AppDelegate.swift
+++ LateLit/AppDelegate.swift
@@ -6,15 +6,17 @@
 //  Copyright © 2020 Daniel Khromov. All rights reserved.
 //
 
-import UIKit
 
+import UIKit
+import Firebase
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
