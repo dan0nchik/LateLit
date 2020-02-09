af46a6e81da50e4931a20fcaa65160da62c50cd2
Daniel Khromov <danielkhromov@gmail.com>
2020-02-10 00:25:10 +0300

    First commit

---
+++ LateLit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LateLit
+//
+//  Created by Daniel Khromov on 2/10/20.
+//  Copyright © 2020 Daniel Khromov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
