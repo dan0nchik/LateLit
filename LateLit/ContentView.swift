17a26ad2e1a99c5cabfd72f1250a5d724022cf8b
Daniel Khromov <danielkhromov@gmail.com>
2020-02-11 20:59:37 +0300

    начальная страница + навигация(аккаунт)

--- LateLit/ContentView.swift
+++ LateLit/ContentView.swift
@@ -8,14 +8,62 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView
+            {
+            
+        VStack
+            {
+             Spacer()
+                LateButton()
+            Spacer()
+                HurryImage()
+                }
+            
+        .navigationBarTitle("LateLit")
+        .navigationBarItems(trailing: NavigationLink(destination: Account(), label: {
+            AccountImage()
+        }))
+        }
     }
 }
-
+                
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct LateButton: View {
+    var body: some View {
+        Button(action:{}) {
+            Text("Я опоздаю")
+                .fontWeight(.semibold)
+                .foregroundColor(Color(.white))
+                .padding(90)
+                .background(Color("Color"))
+                .mask(Circle())
+                .shadow(color: Color("Color"), radius: 10)
+            
+        }
+    }
+}
+
+struct HurryImage: View {
+    var body: some View {
+        Image("hurry")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+    }
+}
+
+struct AccountImage: View {
+    var body: some View {
+        Text("Аккаунт")
+        .foregroundColor(Color("Color"))
+            .fontWeight(.medium)
     }
 }
